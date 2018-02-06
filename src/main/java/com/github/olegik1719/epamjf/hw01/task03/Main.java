da28f0996b52df0bc5ab5fa90780014af47d2361
Oleg Mihaylov <oleg.mihaylov@gmail.com>
2018-02-06 20:08:04 +0300

    Finished hw01.task03

--- src/main/java/com/github/olegik1719/epamjf/hw01/task03/Main.java
+++ src/main/java/com/github/olegik1719/epamjf/hw01/task03/Main.java
@@ -1,7 +1,16 @@
 package com.github.olegik1719.epamjf.hw01.task03;
 
 public class Main {
+
+    private static double a=1;
+    private static double b=2;
+    private static int n=20;
+
     public static void main(String[] args) {
+        for (int i = 0; i <= n ; i++) {
+            double arg = a+i*(b-a)/n;
+            System.out.printf("%1.3f: %.7f%n",arg, expression(arg));
+        }
 
     }
 
