2751e2a1c862df8346e8946301898e7f36ee8054
olegik1719 <oleg.mihaylov@gmail.com>
2018-02-07 00:10:24 +0300

    finished task0105

--- src/main/java/com/github/olegik1719/epamjf/hw01/task05/Main.java
+++ src/main/java/com/github/olegik1719/epamjf/hw01/task05/Main.java
@@ -1,4 +1,15 @@
 package com.github.olegik1719.epamjf.hw01.task05;
 
 public class Main {
+    public static  int[][] getMatrix(int size){
+        int[][] result = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j|| i == size - 1 -j)
+                    result[i][j] = 1;
+                else result[i][j] = 0;
+            }
+        }
+        return result;
+    }
 }
